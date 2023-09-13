27c99d44b7bca94ebee70fefd9d610e08c8e1014
Andrey Fedinyak <fedinyakandrey@gmail.com>
2023-09-13 17:14:43 +0300

    Fix test

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 /* eslint-disable functional/no-expression-statements */
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  // [...]
-  // Replace `ts-jest` with the preset you want to use
-  // from the above list
   preset: 'ts-jest',
+  testEnvironment: 'node',
 };
